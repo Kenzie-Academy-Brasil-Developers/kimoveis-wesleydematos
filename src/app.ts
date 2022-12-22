39fe7cc9efbc1ea8428e63566718ee5ce375fc09
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-12-22 11:03:49 -0300

    fix: add app.use routes login/users and handleError

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
+import { userRoutes } from "./routes/users.routes";
+import handleError from "./errors/handleError";
+import { sessionRoutes } from "./routes/session.routes";
 
 const app = express();
 app.use(express.json());
+app.use("/users", userRoutes);
+app.use("/login", sessionRoutes);
+
+app.use(handleError);
 
 export default app;
