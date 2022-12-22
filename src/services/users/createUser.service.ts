5c1cdf352c253f1da2330d799e8a4ecedc3226a3
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-12-22 10:12:04 -0300

    feat: add createUserService

---
+++ src/services/users/createUser.service.ts
@@ -0,0 +1,27 @@
+import AppDataSource from "../../data-source";
+import { User } from "../../entities/userEntity";
+import { AppError } from "../../errors/AppError";
+import { IUserRequest } from "../../interfaces/users";
+import { userWithOutPasswordSerializer } from "../../serializers/users.serializers";
+
+const createUserService = async (userData: IUserRequest) => {
+  const userRepository = AppDataSource.getRepository(User);
+  const foundUser = await userRepository.findOneBy({ email: userData.email });
+
+  if (!foundUser) {
+    const user = userRepository.create(userData);
+    await userRepository.save(user);
+
+    console.log(user);
+
+    const userWoP = await userWithOutPasswordSerializer.validate(user, {
+      stripUnknown: true,
+    });
+
+    return userWoP;
+  }
+
+  throw new AppError("User already exists!", 409);
+};
+
+export default createUserService;
