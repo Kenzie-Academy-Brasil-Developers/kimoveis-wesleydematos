3f51063ef1c894c3ed168da332f7a3b478406ca3
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-12-30 09:45:13 -0300

    feat: create and use route /categories

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,13 @@ import express from "express";
 import { userRoutes } from "./routes/users.routes";
 import handleError from "./errors/handleError";
 import { sessionRoutes } from "./routes/session.routes";
+import { categoriesRoutes } from "./routes/categories.routes";
 
 const app = express();
 app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/login", sessionRoutes);
+app.use("/categories", categoriesRoutes);
 
 app.use(handleError);
 
---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import {
+  createCategoryController,
+  listCategoriesController,
+  listCategoryWithPropertyByIdController,
+} from "../controllers/categories.controllers";
+import { ensureAuthMiddleWare } from "../middlewares/ensureAuth.middleware";
+import { ensureRequesterIsAdmMiddleware } from "../middlewares/ensureRequesterIsAdm.middleware";
+
+export const categoriesRoutes = Router();
+
+categoriesRoutes.post(
+  "",
+  ensureAuthMiddleWare,
+  ensureRequesterIsAdmMiddleware,
+  createCategoryController
+);
+
+categoriesRoutes.get("", listCategoriesController);
+categoriesRoutes.get("/:id/properties", listCategoryWithPropertyByIdController);
